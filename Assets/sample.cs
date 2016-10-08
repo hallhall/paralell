56d9be3f8dc0779e6968ff50c2607023ff58726e
unknown <>
2016-10-08 16:12:29 +0900

    first_commit2

---
+++ Assets/sample.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class sample : MonoBehaviour {
+
+    public float speed = 0.1f;
+	// Use this for initialization
+	void Start () {
+        UnityEngine.Debug.Log("sample1");
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetKey("right")) {
+            this.transform.position += transform.right * speed;
+        }
+        if (Input.GetKey("left")) {
+            this.transform.position -= transform.right * speed;
+        }
+	}
+}
