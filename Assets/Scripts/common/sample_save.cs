1456e8ca1ac0b2b286d0e4a36650c783cf268fb0
hallhall <haru.soccer15@gmail.com>
2016-10-31 17:38:49 +0900

    for_push

--- Assets/Scripts/common/sample_save.cs
+++ Assets/Scripts/common/sample_save.cs
@@ -3,14 +3,15 @@ using System.Collections;
 
 public class sample_save : MonoBehaviour {
 
-    static int[] have_costume = { 1, 0, 0 }; //have=1, 
-    static int[] have_stage = { 1, 0, 0 };
-    static int[] have_light = { 1, 0, 0 };
-    static int max_score = 0;
-    static int max_combo = 0;
-    static int now_point = 0;
-    static char[] now_book = {'　', '　', '　', '　', '　', '　', '　', '　', '　', '　', '　', '　', '　', '　', '　', '　', '　', '　', '　', '　', '　', '　', '　', '　', '　', '　', '　', '　', '　', '　', '　', '　', '　', '　', '　', '　', '　', '　', '　', '　', '　', '　', '　', '　', '　', '　', '　', '　'};
-    static bool normal_mode = false; //clear=true
-    static bool clasic_mode = false;
-    static bool uniqe_mode = false;
+	public static bool[] have_costume = { true, false, false }; //have=1, 
+	public static bool[] have_stage = { true, false, false };
+	public static bool[] have_light = { true, false, false };
+	public static int max_score = 0;
+	public static int max_combo = 0;
+	public static int now_point = 0;
+	public static char[] now_book = {'　', '　', '　', '　', '　', '　', '　', '　', '　', '　', '　', '　', '　', '　', '　', '　', '　', '　', '　', '　', '　', '　', '　', '　', '　', '　', '　', '　', '　', '　', '　', '　', '　', '　', '　', '　', '　', '　', '　', '　', '　', '　', '　', '　', '　', '　', '　', '　'};
+	public static int now_book_percent = 0;
+	public static bool normal_mode = false; //clear=true
+	public static bool clasic_mode = false;
+	public static bool uniqe_mode = false;
 }
--- Assets/Scripts/ready/ready_game_controller.cs
+++ Assets/Scripts/ready/ready_game_controller.cs
@@ -4,6 +4,7 @@ using common;
 using UnityEngine.UI;
 
 public class ready_game_controller : MonoBehaviour {
+	//unity上のリソース
 
     AudioSource button_bgm;
     AudioSource return_bgm;
@@ -31,39 +32,128 @@ public class ready_game_controller : MonoBehaviour {
     SpriteRenderer have_light_2_img;
 
     GameObject front_wrap;
+	GameObject modal_costume_wrap;
+	GameObject modal_stage_wrap;
+	GameObject modal_light_wrap;
 
-    Particle light_particle;
+	ParticleSystem light_particle;
+
+	//動作用
+	string[] costumes_str = {"alice_body_1", "alice_body_2", "alice_body_3"};
+	string[] stages_str = {"tree_back", "house_back", "building_back"};
+	Color[] lights_str = {new Color(254f/255f, 255f/255f, 193f/255f), new Color(254f/255f, 255f/255f, 193f/255f), new Color(254f/255f, 255f/255f, 193f/255f)};
+	bool is_modal = false; //モーダルが起動中か否か
+
+	//別画面移動用
+	bool is_move = false; //これ付きならば移動
+	string to_move_str = ""; //この先へ移動
 
 	// Use this for initialization
 	void Start () {
-        button_bgm = GameObject.Find("button_bgm").GetComponent<AudioSource>();
-	    return_bgm = GameObject.Find("return_bgm").GetComponent<AudioSource>();
-        stage_score = GameObject.Find("stage_score").GetComponent<Text>();
-        stage_combo = GameObject.Find("stage_combo").GetComponent<Text>();
-        normal_mode = GameObject.Find("normal_mode").GetComponent<Text>();
-        classic_mode = GameObject.Find("classic_mode").GetComponent<Text>();
-        unique_mode = GameObject.Find("unique_mode").GetComponent<Text>();
-        book_percent = GameObject.Find("book_percent").GetComponent<Text>();
-        demo_alice_body = GameObject.Find("demo_alice_body").GetComponent<SpriteRenderer>();
-        demo_back_img = GameObject.Find("demo_back_img").GetComponent<SpriteRenderer>();
-        costume_img = GameObject.Find("costume_img").GetComponent<SpriteRenderer>();
-        stage_img = GameObject.Find("stage_img").GetComponent<SpriteRenderer>();
-        light_img = GameObject.Find("light_img").GetComponent<SpriteRenderer>();
-        now_costume_img = GameObject.Find("now_costume_img").GetComponent<SpriteRenderer>();
-        have_costume_1_img = GameObject.Find("have_costume_1_img").GetComponent<SpriteRenderer>();
-        have_costume_2_img = GameObject.Find("have_costume_2_img").GetComponent<SpriteRenderer>();
-        now_stage_img = GameObject.Find("now_stage_img").GetComponent<SpriteRenderer>();
-        have_stage_1_img = GameObject.Find("have_stage_1_img").GetComponent<SpriteRenderer>();
-        have_stage_2_img = GameObject.Find("have_stage_2_img").GetComponent<SpriteRenderer>();
-        now_light_img = GameObject.Find("now_light_img").GetComponent<SpriteRenderer>();
-        have_light_1_img = GameObject.Find("have_light_1_img").GetComponent<SpriteRenderer>();
-        have_light_2_img = GameObject.Find("have_light_2_img").GetComponent<SpriteRenderer>();
-        front_wrap = GameObject.Find("front_wrap");
-        light_particle = GameObject.Find("demo_light_particle").GetComponent<Particle>();
+		set_variable();
+		set_text();
+
     }
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (is_modal) { //モーダル起動中
+			if (common_method.is_touch_3d ("back_btn_tap")) { //returnボタンタップ
+				is_move = true;
+				to_move_str = "story";
+				Debug.Log ("back");
+			}
+		} else { //モーダル停止
+			
+		}
+
+	}
+
+	//変数セット
+	void set_variable () {
+		button_bgm = GameObject.Find("button_bgm").GetComponent<AudioSource>();
+		return_bgm = GameObject.Find("return_bgm").GetComponent<AudioSource>();
+		stage_score = GameObject.Find("stage_score").GetComponent<Text>();
+		stage_combo = GameObject.Find("stage_combo").GetComponent<Text>();
+		normal_mode = GameObject.Find("normal_mode").GetComponent<Text>();
+		classic_mode = GameObject.Find("classic_mode").GetComponent<Text>();
+		unique_mode = GameObject.Find("unique_mode").GetComponent<Text>();
+		book_percent = GameObject.Find("book_percent").GetComponent<Text>();
+		demo_alice_body = GameObject.Find("demo_alice_body").GetComponent<SpriteRenderer>();
+		demo_back_img = GameObject.Find("demo_back_img").GetComponent<SpriteRenderer>();
+		costume_img = GameObject.Find("costume_img").GetComponent<SpriteRenderer>();
+		stage_img = GameObject.Find("stage_img").GetComponent<SpriteRenderer>();
+		light_img = GameObject.Find("light_img").GetComponent<SpriteRenderer>();
+		now_costume_img = GameObject.Find("now_costume_img").GetComponent<SpriteRenderer>();
+		have_costume_1_img = GameObject.Find("have_costume_1_img").GetComponent<SpriteRenderer>();
+		have_costume_2_img = GameObject.Find("have_costume_2_img").GetComponent<SpriteRenderer>();
+		now_stage_img = GameObject.Find("now_stage_img").GetComponent<SpriteRenderer>();
+		have_stage_1_img = GameObject.Find("have_stage_1_img").GetComponent<SpriteRenderer>();
+		have_stage_2_img = GameObject.Find("have_stage_2_img").GetComponent<SpriteRenderer>();
+		now_light_img = GameObject.Find("now_light_img").GetComponent<SpriteRenderer>();
+		have_light_1_img = GameObject.Find("have_light_1_img").GetComponent<SpriteRenderer>();
+		have_light_2_img = GameObject.Find("have_light_2_img").GetComponent<SpriteRenderer>();
+		front_wrap = GameObject.Find("front_wrap");
+		light_particle = GameObject.Find("demo_light_particle").GetComponent<ParticleSystem>();
+		modal_costume_wrap = GameObject.Find ("modal_costume_wrap");
+		modal_costume_wrap.SetActive (false);
+		modal_stage_wrap = GameObject.Find ("modal_stage_wrap");
+		modal_stage_wrap.SetActive (false);
+		modal_light_wrap = GameObject.Find ("modal_light_wrap");
+		modal_light_wrap.SetActive (false);
+	}
+
+	//テキストセット
+	void set_text () {
+		stage_score.text = "Max Score    : " + sample_save.max_score;
+		stage_combo.text = "Max Combo : " + sample_save.max_combo;
+		if (sample_save.normal_mode) normal_mode.color = Color.black;
+		if (sample_save.clasic_mode) classic_mode.color = Color.black;
+		if (sample_save.uniqe_mode) unique_mode.color = Color.black;
+		book_percent.text = sample_save.now_book_percent + " %";
+	}
+
+	//モーダルセット（データのセット)
+	void set_mordal () {
+		//コスチューム
+		if (!sample_save.have_costume [1]) {
+			costumes_str [1] = null;
+			have_costume_1_img.sprite = null;
+			GameObject.Find ("have_costume_1_tap").SetActive (false);
+		}
+		if (!sample_save.have_costume [2]) {
+			costumes_str [2] = null;
+			have_costume_2_img.sprite = null;
+			GameObject.Find ("have_costume_2_tap").SetActive (false);
+		}
+
+		//ステージ
+		if (!sample_save.have_stage [1]) {
+			stages_str [1] = null;
+			have_stage_1_img.sprite = null;
+			GameObject.Find ("have_stage_1_tap").SetActive (false);
+		}
+		if (!sample_save.have_stage [2]) {
+			stages_str [2] = null;
+			have_stage_2_img.sprite = null;
+			GameObject.Find ("have_stage_2_tap").SetActive (false);
+		}
+
+		//ライト
+		if (!sample_save.have_light [1]) {
+			lights_str [1] = Color.clear; //nullだとエラー
+			have_light_1_img.sprite = null;
+			GameObject.Find ("have_lihgt_1_tap").SetActive (false);
+		}
+		if (!sample_save.have_light [2]) {
+			lights_str [2] = Color.clear;
+			have_light_2_img.sprite = null;
+			GameObject.Find ("have_lihgt_2_tap").SetActive (false);
+		}
+	}
+
+	//モーダルウィンドウの画像セット
+	void set_mordal_img () {
+
 	}
 }
